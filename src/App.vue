b2cee6acf398d35d8a4eac395a252a59b0865e7b
Tom Scharstein <trscharstein@gmail.com>
2023-04-01 09:25:12 -0700

    Changing to Button-Group

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,15 @@
 <template>
   <div id="app">
     <Menus refresh />
-    <Panel debug>
+    <Panel>
       <Anno v-if="error">HostAdapter not installed</Anno>
-      <Anno v-else>{{ this.res }}</Anno>
+      <!-- <Anno v-else>{{ this.res }}</Anno> -->
+      <!-- <Select label="Risoluzione" v-model="activeValue" :items="resolutions" @update="updateValue" /> -->
+      <Button-Group label='Risoluzione' exclusive :active="activeIndex" @update="updateIndex">
+        <Button label="72ppi" :color="activeIndex == 0 ? '#59e' : 'var(--color-default)'" ref="btn0" />
+        <Button label="150ppi" :color="activeIndex == 1 ? '#59e' : 'var(--color-default)'" ref="btn1" />
+        <Button label="300ppi" :color="activeIndex == 2 ? '#59e' : 'var(--color-default)'" ref="btn2" />
+      </Button-Group>
     </Panel>
   </div>
 </template>
@@ -13,22 +19,105 @@ import { evalScript } from "brutalism";
 export default {
   data: () => ({
     res: 72,
-    error: false
+    resolutions: [
+      { value: '72', label: 'Video (72 ppi)' },
+      { value: '150', label: 'Media (150 ppi)' },
+      { value: '300', label: 'Alta (300 ppi)' }
+    ],
+    activeValue: '72',
+    override: false,
+    error: false,
+    isMounted: false,
+    activeIndex: 0,
   }),
   async mounted() {
     const self = this;
     console.log(AIEventAdapter)
-    self.res = Number(await evalScript(`(function() { return app.activeDocument.rasterEffectSettings.resolution }())`));
-    AIEventAdapter.getInstance().addEventListener(
+    self.activeValue = await evalScript(`(function() { return app.activeDocument.rasterEffectSettings.resolution }())`) + ""
+    const adapter = AIEventAdapter.getInstance()
+    adapter.addEventListener(
       AIEvent.ART_SELECTION_CHANGED,
-      async (e) => (
-        self.res = Number(await evalScript(`(function() { return app.activeDocument.rasterEffectSettings.resolution }())`))
-      )
+      async (e) => {
+        self.override = true;
+        self.activeValue = await evalScript(`(function() { return app.activeDocument.rasterEffectSettings.resolution }())`) + ""
+        self.$nextTick(() => {
+          self.override = false;
+        })
+      }
+
     );
+    adapter.addEventListener(
+      AIEvent.MENU_CHANGED,
+      async (e) => {
+        self.override = true;
+        self.activeValue = await evalScript(`(function() { return app.activeDocument.rasterEffectSettings.resolution }())`) + ""
+        self.$nextTick(() => {
+          self.override = false;
+        })
+      }
+    );
+    this.isMounted = true;
+    this.$nextTick(() => {
+      self.flushActiveElts()
+    })
+    // 
+  },
+  watch: {
+    activeValue(value) {
+      console.log(value);
+      this.activeIndex = [72, 150, 300].findIndex(i => +i == +value);
+      if (this.isMounted) {
+        this.flushActiveElts()
+      }
+    },
   },
   methods: {
+    flushActiveElts() {
+      [0, 1, 2].forEach(i => {
+        const ref = this.$refs[`btn${i}`].$el
+        if (+i !== +this.activeIndex) {
+          ref.classList.remove('active');
+        } else {
+          ref.classList.add('active');
+        }
+      })
+    },
+    async updateValue(value) {
+      const self = this;
+      if (!self.override) {
+        console.log('UPDATE:', value);
+        self.override = true;
+        await evalScript(`runActionFromString('${value}ppi', 'resolution')`);
+        setTimeout(() => {
+          self.override = false;
+        }, 500);
+      }
+    },
+    async updateIndex(value) {
+      const self = this;
+      const reso = [
+        72, 150, 300
+      ]
+      if (!self.override) {
+        self.activeIndex = value;
+        const e = reso[value];
+        console.log('UPDATE:', value);
+        self.override = true;
+        await evalScript(`runActionFromString('${e}ppi', 'resolution')`);
+        setTimeout(() => {
+          self.override = false;
+        }, 500);
+      }
+    }
   },
 };
 </script>
 
-<style></style>
+<style>
+.app,
+.panel,
+html,
+body {
+  overflow-y: hidden;
+}
+</style>
--- src/host/ILST/host.jsx
+++ src/host/ILST/host.jsx
@@ -1,1 +1,238 @@
-console.log('Host is online');
+var actionString =
+  "/version 3" +
+  "/name [ 10" +
+  "	7265736f6c7574696f6e" +
+  "]" +
+  "/isOpen 1" +
+  "/actionCount 3" +
+  "/action-1 {" +
+  "	/name [ 6" +
+  "		333030707069" +
+  "	]" +
+  "	/keyIndex 0" +
+  "	/colorIndex 0" +
+  "	/isOpen 1" +
+  "	/eventCount 1" +
+  "	/event-1 {" +
+  "		/useRulersIn1stQuadrant 0" +
+  "		/internalName (ai_plugin_rasterEffectSettings)" +
+  "		/localizedName [ 32" +
+  "			446f63756d656e742052617374657220456666656374732053657474696e6773" +
+  "		]" +
+  "		/isOpen 1" +
+  "		/isOn 1" +
+  "		/hasDialog 1" +
+  "		/showDialog 0" +
+  "		/parameterCount 7" +
+  "		/parameter-1 {" +
+  "			/key 1668246642" +
+  "			/showInPalette -1" +
+  "			/type (enumerated)" +
+  "			/name [ 4" +
+  "				434d594b" +
+  "			]" +
+  "			/value 5" +
+  "		}" +
+  "		/parameter-2 {" +
+  "			/key 1685088558" +
+  "			/showInPalette -1" +
+  "			/type (integer)" +
+  "			/value 300" +
+  "		}" +
+  "		/parameter-3 {" +
+  "			/key 1651205988" +
+  "			/showInPalette -1" +
+  "			/type (enumerated)" +
+  "			/name [ 11" +
+  "				5472616e73706172656e74" +
+  "			]" +
+  "			/value 1" +
+  "		}" +
+  "		/parameter-4 {" +
+  "			/key 1634494835" +
+  "			/showInPalette -1" +
+  "			/type (boolean)" +
+  "			/value 0" +
+  "		}" +
+  "		/parameter-5 {" +
+  "			/key 1835103083" +
+  "			/showInPalette -1" +
+  "			/type (boolean)" +
+  "			/value 0" +
+  "		}" +
+  "		/parameter-6 {" +
+  "			/key 1886613620" +
+  "			/showInPalette -1" +
+  "			/type (boolean)" +
+  "			/value 0" +
+  "		}" +
+  "		/parameter-7 {" +
+  "			/key 1885430884" +
+  "			/showInPalette -1" +
+  "			/type (unit real)" +
+  "			/value 36.0" +
+  "			/unit 592476268" +
+  "		}" +
+  "	}" +
+  "}" +
+  "/action-2 {" +
+  "	/name [ 6" +
+  "		313530707069" +
+  "	]" +
+  "	/keyIndex 0" +
+  "	/colorIndex 0" +
+  "	/isOpen 1" +
+  "	/eventCount 1" +
+  "	/event-1 {" +
+  "		/useRulersIn1stQuadrant 0" +
+  "		/internalName (ai_plugin_rasterEffectSettings)" +
+  "		/localizedName [ 32" +
+  "			446f63756d656e742052617374657220456666656374732053657474696e6773" +
+  "		]" +
+  "		/isOpen 1" +
+  "		/isOn 1" +
+  "		/hasDialog 1" +
+  "		/showDialog 0" +
+  "		/parameterCount 7" +
+  "		/parameter-1 {" +
+  "			/key 1668246642" +
+  "			/showInPalette -1" +
+  "			/type (enumerated)" +
+  "			/name [ 4" +
+  "				434d594b" +
+  "			]" +
+  "			/value 5" +
+  "		}" +
+  "		/parameter-2 {" +
+  "			/key 1685088558" +
+  "			/showInPalette -1" +
+  "			/type (integer)" +
+  "			/value 150" +
+  "		}" +
+  "		/parameter-3 {" +
+  "			/key 1651205988" +
+  "			/showInPalette -1" +
+  "			/type (enumerated)" +
+  "			/name [ 11" +
+  "				5472616e73706172656e74" +
+  "			]" +
+  "			/value 1" +
+  "		}" +
+  "		/parameter-4 {" +
+  "			/key 1634494835" +
+  "			/showInPalette -1" +
+  "			/type (boolean)" +
+  "			/value 0" +
+  "		}" +
+  "		/parameter-5 {" +
+  "			/key 1835103083" +
+  "			/showInPalette -1" +
+  "			/type (boolean)" +
+  "			/value 0" +
+  "		}" +
+  "		/parameter-6 {" +
+  "			/key 1886613620" +
+  "			/showInPalette -1" +
+  "			/type (boolean)" +
+  "			/value 0" +
+  "		}" +
+  "		/parameter-7 {" +
+  "			/key 1885430884" +
+  "			/showInPalette -1" +
+  "			/type (unit real)" +
+  "			/value 36.0" +
+  "			/unit 592476268" +
+  "		}" +
+  "	}" +
+  "}" +
+  "/action-3 {" +
+  "	/name [ 5" +
+  "		3732707069" +
+  "	]" +
+  "	/keyIndex 0" +
+  "	/colorIndex 0" +
+  "	/isOpen 1" +
+  "	/eventCount 1" +
+  "	/event-1 {" +
+  "		/useRulersIn1stQuadrant 0" +
+  "		/internalName (ai_plugin_rasterEffectSettings)" +
+  "		/localizedName [ 32" +
+  "			446f63756d656e742052617374657220456666656374732053657474696e6773" +
+  "		]" +
+  "		/isOpen 1" +
+  "		/isOn 1" +
+  "		/hasDialog 1" +
+  "		/showDialog 0" +
+  "		/parameterCount 7" +
+  "		/parameter-1 {" +
+  "			/key 1668246642" +
+  "			/showInPalette -1" +
+  "			/type (enumerated)" +
+  "			/name [ 4" +
+  "				434d594b" +
+  "			]" +
+  "			/value 5" +
+  "		}" +
+  "		/parameter-2 {" +
+  "			/key 1685088558" +
+  "			/showInPalette -1" +
+  "			/type (integer)" +
+  "			/value 72" +
+  "		}" +
+  "		/parameter-3 {" +
+  "			/key 1651205988" +
+  "			/showInPalette -1" +
+  "			/type (enumerated)" +
+  "			/name [ 11" +
+  "				5472616e73706172656e74" +
+  "			]" +
+  "			/value 1" +
+  "		}" +
+  "		/parameter-4 {" +
+  "			/key 1634494835" +
+  "			/showInPalette -1" +
+  "			/type (boolean)" +
+  "			/value 0" +
+  "		}" +
+  "		/parameter-5 {" +
+  "			/key 1835103083" +
+  "			/showInPalette -1" +
+  "			/type (boolean)" +
+  "			/value 0" +
+  "		}" +
+  "		/parameter-6 {" +
+  "			/key 1886613620" +
+  "			/showInPalette -1" +
+  "			/type (boolean)" +
+  "			/value 0" +
+  "		}" +
+  "		/parameter-7 {" +
+  "			/key 1885430884" +
+  "			/showInPalette -1" +
+  "			/type (unit real)" +
+  "			/value 36.0" +
+  "			/unit 592476268" +
+  "		}" +
+  "	}" +
+  "}" +
+  "";
+
+/**
+ * Creates and runs a temporary action from given .aia rawtext
+ * @param {String} actionName Exact name of action
+ * @param {String} actionSet Exact name of set containing action
+ * @param {String} contents Raw .aia string of action file
+ */
+function runActionFromString(actionName, actionSet) {
+  // Rather oddly AI always wants you to use a file instead of a string,
+  // so we'll just create a temp file every time we need to:
+  var tmp = File(Folder.desktop + "/tmp.aia");
+  tmp.open("w");
+  tmp.write(actionString);
+  tmp.close();
+  app.loadAction(tmp);
+  app.doScript(actionName, actionSet, false);
+  app.unloadAction(actionSet, "");
+  // And remove the file:
+  tmp.remove();
+}
